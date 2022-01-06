c66ae66562279987c00723fadcacb0ee746d3ebc
Sarah Faber <31863074+fabsarah@users.noreply.github.com>
2022-01-06 14:10:49 -0400

    Add files via upload

---
+++ shower_tile_plot.m
@@ -0,0 +1,16 @@
+function s = shower_tile_plot(inmat)
+% A function to plot matrix data with neat lines between everything like
+% pcolor...but with all the weird intermediary steps necessary to make sure
+% everything's the right way up, and that we don't lose a row/column of
+% data. For plotting PLS results, reshape and threshold them before getting
+% here. I haven't included syntax for axis ticks, labelling, or titles, so
+% also do those. 
+%%
+nrows = size(inmat,1);
+ncols = size(inmat,2);
+inmat = flipud(inmat);
+inmat(nrows+1,1:ncols) = nan;
+inmat(:,ncols+1) = nan;
+s = pcolor(inmat);
+s.LineWidth = 1.5;
+colorbar
